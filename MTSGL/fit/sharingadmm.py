sharingadmm implemented, wrong results for now

MTSGL/fit/sharingadmm.py
@@ -59,9 +59,9 @@ class SharingADMM(Fit):
 		"""
 		# initialization
 		beta = beta
-		eta = {task: loss.lin_predictor(beta[:, [k]]) for k, (task, loss) in enumerate(self.loss.iteritems())}
-		z = {task: np.zeros_like(eta[task]) for k, (task, loss) in enumerate(self.loss.iteritems())}
-		r = {task: etak - loss[task].lin_predictor(beta) for task, (etak, loss) in dict_zip(eta, self.loss).items()}
+		eta = {task: loss.lin_predictor(beta[:, [k]]) for k, (task, loss) in enumerate(self.loss.items())}
+		z = {task: np.zeros_like(eta[task]) for k, (task, loss) in enumerate(self.loss.items())}
+		r = {task: etak - loss.lin_predictor(beta) for task, (etak, loss) in dict_zip(eta, self.loss).items()}
 		t = 0
 		# setup logging
 		dual_obj, primal_obj = self.compute_obj(beta, eta, lam, r, z)
@@ -71,14 +71,14 @@ class SharingADMM(Fit):
 		while True:
 			t += 1
 			# update eta
-			eta, _ = ridge_saturated(
+			eta, n_ridge = ridge_saturated(
 				loss=self.loss,
 				z0=eta,
-				a={task: self.loss[task].lin_predictor(beta) - z[task] for task in self.loss},
+				a={task: loss.lin_predictor(beta[:, [k]]) - z[task] for k, (task, loss) in enumerate(self.loss.items())},
 				tau=self.rho
 			)
 			# update beta
-			beta = proxgd(
+			beta, n_proxgd = proxgd(
 				loss=self.loss,
 				reg=self.reg,
 				beta0=beta,
@@ -86,15 +86,16 @@ class SharingADMM(Fit):
 				lam=lam,
 				rho=self.rho
 			)
+			print(lam, t, n_ridge, n_proxgd)
 			# update z and r
-			r = {task: etak - loss.lin_predictor(beta) for task, (etak, loss) in dict_zip(eta, self.loss).items()}
+			r = {task: eta[task] - loss.lin_predictor(beta[:, [k]]) for k, (task, loss) in enumerate(self.loss.items())}
 			z = {task: zk + rk for task, (zk, rk) in dict_zip(z, r).items()}
 			# norms for convergence
-			r_norm = {task: np.linalg.norm(rk, 2) for task, rk in r.items()}
-			s_norm = {
-				task: self.rho * np.linalg.norm(np.matmul(loss.x.T, rk), 2)
+			r_norm = [np.linalg.norm(rk, 2) for task, rk in r.items()]
+			s_norm = [
+				self.rho * np.linalg.norm(np.matmul(loss.x.T, rk), 2)
 				for task, (rk, loss) in dict_zip(r, self.loss).items()
-			}
+			]
 			xbeta_norm = np.sqrt(sum([
 				np.linalg.norm(loss.lin_predictor(beta[:, [k]]), 2) ** 2
 				for k, loss in enumerate(self.loss.values())
@@ -107,8 +108,7 @@ class SharingADMM(Fit):
 				np.linalg.norm(np.matmul(loss.x.T, zk), 2) ** 2
 				for loss, zk in dict_zip(self.loss, z).values()
 			]))
-
-
+			# compute thresholds
 			eps_primal = np.sqrt(sum(self.loss.data.n_obs.values())) * self.eps_abs + \
 				self.eps_rel * max(xbeta_norm, eta_norm)
 			eps_dual = np.sqrt(self.loss.data.n_features * self.loss.data.n_tasks) * self.eps_abs + \
@@ -121,7 +121,7 @@ class SharingADMM(Fit):
 				),
 				ignore_index=True
 			)
-			if r_norm < eps_primal and s_norm < eps_dual:
+			if np.linalg.norm(np.array(r_norm), 2) < eps_primal and np.linalg.norm(np.array(s_norm), 2) < eps_dual:
 				break
 			if t > self.max_iter:
 				print(self.log_solve)

MTSGL/fit/sharingadmm_utils.py
@@ -110,20 +110,20 @@ def ridge_saturated(
 	while True:
 		t += 1
 		z_prev = zt
-		yt = {task: zt[task] + (zt[task] - ztm1[task]) * (t - 1) / (t + 2) for task in loss.keys()}
+		yt = {task: zt[task] + (zt[task] - ztm1[task]) * (t - 1) / (t + 2) for task in loss}
 		grad = loss.gradient_saturated(yt)
 		grad = {task: g + (yt[task] - a[task]) / tau for task, g in grad.items()}
-		products = {task: np.matmul(grad[task].transpose(), zt[task] - ztm1[task]) for task in loss.keys()}
+		products = {task: np.matmul(grad[task].transpose(), zt[task] - ztm1[task]) for task in loss}
 		if sum(products.values()) > 0.:
 			# do a regular gd step
 			grad = loss.gradient_saturated(zt)
 			grad = {task: g + (zt[task] - a[task]) / tau for task, g in grad.items()}
-			zt = {task: zt[task] - grad[task] * step_size for task in loss.keys()}
+			zt = {task: zt[task] - grad[task] * step_size for task in loss}
 		else:
 			# use momentum
-			zt = {task: yt[task] - grad[task] * step_size for task in loss.keys()}
+			zt = {task: yt[task] - grad[task] * step_size for task in loss}
 		ztm1 = z_prev
-		step = np.sqrt(sum([np.linalg.norm(zt[task] - z_prev[task], 2) ** 2 for task in loss.keys()]))
+		step = np.sqrt(sum([np.linalg.norm(zt[task] - z_prev[task], 2) ** 2 for task in loss]))
 		if t >= max_iter:
 			raise RuntimeError("ridge did not converge in {} iterations".format(t))
 		if step < threshold:

MTSGL/losses/mtloss.py
@@ -76,6 +76,12 @@ class SeparableMTLoss(MTLoss):
 	def __getitem__(self, task):
 		return self._losses[task]
 
+	def get(self, task, default=None):
+		if task in self:
+			return self[task]
+		else:
+			return default
+
 	def __repr__(self):
 		return repr(self._losses)
 
@@ -124,11 +130,11 @@ class SeparableMTLoss(MTLoss):
 				beta = np.zeros((self.data.n_features, 1))
 			return self[task].loss(beta)
 
-	def loss_from_linear_predictor(self, eta: Dict[str, np.ndarray], task: Optional[str]):
+	def loss_from_linear_predictor(self, eta: Dict[str, np.ndarray], task: Optional[str] = None):
 		if task is None:
 			if eta is None:
-				eta = {task: np.zeros_like(self.loss[task].y) for task in self.loss}
-			return sum([loss.loss_from_linear_predictor(eta[task]) for task, loss in self.iteritems()])
+				eta = {task: np.zeros_like(loss.y) for task, loss in self.loss.items()}
+			return sum([loss.loss_from_linear_predictor(eta[task]) for task, loss in self.items()])
 		else:
 			if eta is None:
 				eta = np.zeros_like(self.loss[task].y)

scratches/ADMM.py
@@ -41,15 +41,23 @@ weights[0] = 0.
 
 reg = MTSGL.regularizations.SparseGroupLasso(q=2, alpha=0.5, weights=weights)
 
-model = MTSGL.fit.ConsensusADMM(
+model_sharing = MTSGL.fit.SharingADMM(
 	loss, reg, n_lam=100, lam_frac=0.001, rho=1, max_iter=10000, verbose=1
 )
 
+
+model_consensus = MTSGL.fit.ConsensusADMM(
+	loss, reg, n_lam=100, lam_frac=0.001, rho=1, max_iter=10000, verbose=1
+)
+
+
+
+
+model = model_consensus
+
+model = model_sharing
+
 beta_norm = np.apply_along_axis(lambda x: max(np.abs(x)), 2, model.path)
-
-
-
-
 
 N = 256
 vals = np.ones((N, 4))
@@ -76,3 +84,4 @@ plt.imshow(beta_norm, cmap='inferno', aspect='auto')
 plt.colorbar()
 plt.show()
 
+model_consensus.path[25, :] - model_sharing.path[-1, :]